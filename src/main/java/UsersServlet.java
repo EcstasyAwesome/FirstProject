c884d58437404ea3709654c23ac42fd4bee68092
EcstasyAwesome <ecstasy.awesome@gmail.com>
2017-10-21 20:12:49 +0300

    -

--- src/main/java/UsersServlet.java
+++ src/main/java/UsersServlet.java
@@ -35,11 +35,15 @@ public class UsersServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             String method = req.getParameter("method");
-            if (method != null && method.equals("PUT")) {
-                doPut(req, resp);
-            }
-            if (method != null && method.equals("DELETE")) {
-                doDelete(req, resp);
+            if (method != null) {
+                switch (method) {
+                    case "PUT":
+                        doPut(req, resp);
+                        break;
+                    case "DELETE":
+                        doDelete(req, resp);
+                        break;
+                }
             }
         } catch (Exception e) {
             System.out.println(e);
@@ -60,37 +64,39 @@ public class UsersServlet extends HttpServlet {
             String name;
             String description;
             if (form != null) {
-                if (form.equals("addUser")) {
-                    surname = new String(req.getParameter("user_surname").getBytes("ISO-8859-1"), "UTF-8");
-                    firstName = new String(req.getParameter("user_firstName").getBytes("ISO-8859-1"), "UTF-8");
-                    secondName = new String(req.getParameter("user_secondName").getBytes("ISO-8859-1"), "UTF-8");
-                    phoneNumber = Long.parseLong(req.getParameter("user_phoneNumber"));
-                    position = Integer.parseInt(req.getParameter("position_id"));
-                    new UserDAO(connection).addUser(surname, firstName, secondName, phoneNumber, position);
-                    resp.sendRedirect("/company/users");
-                }
-                if (form.equals("updateUser")) {
-                    surname = new String(req.getParameter("user_surname").getBytes("ISO-8859-1"), "UTF-8");
-                    firstName = new String(req.getParameter("user_firstName").getBytes("ISO-8859-1"), "UTF-8");
-                    secondName = new String(req.getParameter("user_secondName").getBytes("ISO-8859-1"), "UTF-8");
-                    phoneNumber = Long.parseLong(req.getParameter("user_phoneNumber"));
-                    position = Integer.parseInt(req.getParameter("position_id"));
-                    id = Integer.parseInt(req.getParameter("user_id"));
-                    new UserDAO(connection).updateUser(id, surname, firstName, secondName, phoneNumber, position);
-                    resp.sendRedirect("/company/users?key=user_id&value=" + id);
-                }
-                if (form.equals("addPosition")) {
-                    name = new String(req.getParameter("position_name").getBytes("ISO-8859-1"), "UTF-8");
-                    description = new String(req.getParameter("position_description").getBytes("ISO-8859-1"), "UTF-8");
-                    new PositionDAO(connection).addPosition(name, description);
-                    resp.sendRedirect("/company/positions");
-                }
-                if (form.equals("updatePosition")) {
-                    id = Integer.parseInt(req.getParameter("position_id"));
-                    name = new String(req.getParameter("position_name").getBytes("ISO-8859-1"), "UTF-8");
-                    description = new String(req.getParameter("position_description").getBytes("ISO-8859-1"), "UTF-8");
-                    new PositionDAO(connection).updatePosition(id, name, description);
-                    resp.sendRedirect("/company/positions");
+                switch (form) {
+                    case "addUser":
+                        surname = new String(req.getParameter("user_surname").getBytes("ISO-8859-1"), "UTF-8");
+                        firstName = new String(req.getParameter("user_firstName").getBytes("ISO-8859-1"), "UTF-8");
+                        secondName = new String(req.getParameter("user_secondName").getBytes("ISO-8859-1"), "UTF-8");
+                        phoneNumber = Long.parseLong(req.getParameter("user_phoneNumber"));
+                        position = Integer.parseInt(req.getParameter("position_id"));
+                        new UserDAO(connection).addUser(surname, firstName, secondName, phoneNumber, position);
+                        resp.sendRedirect("/company/users");
+                        break;
+                    case "updateUser":
+                        surname = new String(req.getParameter("user_surname").getBytes("ISO-8859-1"), "UTF-8");
+                        firstName = new String(req.getParameter("user_firstName").getBytes("ISO-8859-1"), "UTF-8");
+                        secondName = new String(req.getParameter("user_secondName").getBytes("ISO-8859-1"), "UTF-8");
+                        phoneNumber = Long.parseLong(req.getParameter("user_phoneNumber"));
+                        position = Integer.parseInt(req.getParameter("position_id"));
+                        id = Integer.parseInt(req.getParameter("user_id"));
+                        new UserDAO(connection).updateUser(id, surname, firstName, secondName, phoneNumber, position);
+                        resp.sendRedirect("/company/users?key=user_id&value=" + id);
+                        break;
+                    case "addPosition":
+                        name = new String(req.getParameter("position_name").getBytes("ISO-8859-1"), "UTF-8");
+                        description = new String(req.getParameter("position_description").getBytes("ISO-8859-1"), "UTF-8");
+                        new PositionDAO(connection).addPosition(name, description);
+                        resp.sendRedirect("/company/positions");
+                        break;
+                    case "updatePosition":
+                        id = Integer.parseInt(req.getParameter("position_id"));
+                        name = new String(req.getParameter("position_name").getBytes("ISO-8859-1"), "UTF-8");
+                        description = new String(req.getParameter("position_description").getBytes("ISO-8859-1"), "UTF-8");
+                        new PositionDAO(connection).updatePosition(id, name, description);
+                        resp.sendRedirect("/company/positions");
+                        break;
                 }
             }
         } catch (Exception e) {
@@ -105,15 +111,17 @@ public class UsersServlet extends HttpServlet {
             String form = req.getParameter("form");
             int id;
             if (form != null) {
-                if (form.equals("deleteUser")) {
-                    id = Integer.parseInt(req.getParameter("user_id"));
-                    new UserDAO(connection).deleteUser(id);
-                    resp.sendRedirect("/company/users");
-                }
-                if (form.equals("deletePosition")) {
-                    id = Integer.parseInt(req.getParameter("position_id"));
-                    new PositionDAO(connection).deletePosition(id);
-                    resp.sendRedirect("/company/positions");
+                switch (form) {
+                    case "deleteUser":
+                        id = Integer.parseInt(req.getParameter("user_id"));
+                        new UserDAO(connection).deleteUser(id);
+                        resp.sendRedirect("/company/users");
+                        break;
+                    case "deletePosition":
+                        id = Integer.parseInt(req.getParameter("position_id"));
+                        new PositionDAO(connection).deletePosition(id);
+                        resp.sendRedirect("/company/positions");
+                        break;
                 }
             }
         } catch (Exception e) {
